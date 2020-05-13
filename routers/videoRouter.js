5eb9d0fcfcb5e70465ad6d8e055629dc9ad6c701
aleatorik <vates0328@gmail.com>
2020-05-13 22:35:20 +0900

    initial project

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -4,17 +4,25 @@ import {
   getUpload,
   postUpload,
   videoDetail,
-  editVideo,
   deleteVideo,
+  getEditVideo,
+  postEditVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-//videoRouter.get(routes.videos, videos);
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
