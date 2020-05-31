3532c18f9e223a2fd42f33bdd7c45f81398bd051
aleatorik <vates0328@gmail.com>
2020-05-31 22:35:11 +0900

    initial project

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
 
-app.use(routes.home, globalRouter); //home/join/search
+app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
