98f7353fcaead07700978bb4f52ddca0ce04c952
aleatorik <vates0328@gmail.com>
2020-06-02 21:24:02 +0900

    initial project

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,12 @@
+import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
 
 export const getJoin = (req, res) => {
+  // 누군가를 사용자로 가입시키는 기능
   res.render("join", { pageTitle: "Join" });
 };
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 },
   } = req;
@@ -18,8 +20,10 @@ export const postJoin = async (req, res) => {
         email,
       });
       await User.register(user, password);
+      next();
     } catch (error) {
       console.log(error);
+      res.redirect(routes.home);
     }
     //        2)Log user in
     res.redirect(routes.home);
@@ -28,9 +32,10 @@ export const postJoin = async (req, res) => {
 
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home,
+});
 
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
