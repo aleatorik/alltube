d0a3ae3f7e74ef7eda6fed651437e551e96b40c3
aleatorik <vates0328@gmail.com>
2020-06-02 22:38:45 +0900

    initial project

--- middlewares.js
+++ middlewares.js
@@ -7,6 +7,8 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "AllTube";
   res.locals.routes = routes;
   res.locals.user = req.user || {};
+  res.locals.user = req.user || null;
+  console.log(req.user);
   next();
 };
 
