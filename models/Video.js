1f26beeb074c277fa0b8d9b6203026b35d04dbc7
aleatorik <vates0328@gmail.com>
2020-06-03 20:50:18 +0900

    initial project

--- models/Video.js
+++ models/Video.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const VideoSchema = new mongoose.Schema({
   fileUrl: {
     type: String,
-    required: "File URL is required", //fileURL값이 없는 video를 생성하려 할 때 이 error 메세지를 받음
+    required: "File URL is required",
   },
   title: {
     type: String,
-    required: "Title is required",
+    required: "Tilte is required",
   },
   description: String,
   views: {
@@ -24,6 +24,10 @@ const VideoSchema = new mongoose.Schema({
       ref: "Comment",
     },
   ],
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const model = mongoose.model("Video", VideoSchema);
